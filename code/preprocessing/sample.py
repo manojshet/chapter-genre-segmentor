8f6723202725352fa55f3a1fdd9715f1b4208baa
Manoj Shet <manojjs168@gmail.com>
2022-01-31 22:33:14 -0500

    DSF project

--- code/preprocessing/sample.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-if __name__ == '__main__':
-    df = pd.read_csv("../dataset/preprocessed/01_cleaned.csv",error_bad_lines=False)
-    print(len(df.Genres.value_counts().index.to_list()))
